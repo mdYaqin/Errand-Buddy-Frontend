3c20b5bc233f23b10ec3d39439fe23f65cca1421
Michael Kalamogan <michaelkalamogan@gmail.com>
2021-07-16 11:23:17 +0800

    added like buttons

--- errand-buddy-fe/src/components/Sections/Likes.jsx
+++ errand-buddy-fe/src/components/Sections/Likes.jsx
@@ -1,9 +1,72 @@
-import React from 'react';
+import userEvent from '@testing-library/user-event';
+import React, { useState, useEffect } from "react";
+import Axios from 'axios';
 
-function Likes() {
+function Likes(props) {
+
+    const variable = { errandId: props.errandId, userId: props.userId}
+    const [Likes, setLikes] = useState(0)
+    const [LikeAction, setLikeAction] = useState(null)
+
+    useEffect( () => {
+
+        Axios.post('http://localhost:4000/api/errands/like', variable)
+        .then(response => {
+            if(response.data.success) {
+                
+                setLikes(response.data.likes.length)
+
+                response.data.likes.map(like => {
+                    if(like.userID === props.userId) {
+                        setLikeAction('liked')
+                    }
+                })
+            } else {
+                alert('failed to get likes')
+            }
+        })
+    }, [])
+
+    const onLike = () => {
+        
+        if(LikeAction === null)  {
+
+            Axios.post('http://localhost:4000/api/errands/like/addLike', variable)
+            .then(response => {
+                if (response.data.success) {
+
+                    setLikes(Likes + 1)
+                    setLikeAction('liked')
+
+                } else {
+                    alert('failed to add like')
+                }
+            })
+
+        } else {
+
+            Axios.post('http://localhost:4000/api/errands/like/removeLike', variable)
+            .then(response => {
+                if (response.data.success) {
+
+                    setLikes(Likes - 1)
+                    setLikeAction(null)
+
+                } else {
+                    alert('failed to remove like')
+                }
+            })
+        }
+    }
+
     return (
         <div className="card-like">
-            <i class="far fa-heart"></i>
+            <i className="far fa-heart" theme ={ LikeAction === 'liked' ? 'filled' : 'outlined'}
+            onClick={onLike}
+            >
+
+            </i>
+            <span>{Likes}</span>
         </div>
     );
 }
