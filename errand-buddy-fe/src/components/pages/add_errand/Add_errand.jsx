80456ff4be1a4737687367c71679d56d645931ab
mdYaqin <abu_naal@hotmail.com>
2021-07-28 09:12:27 +0800

    readme template

--- errand-buddy-fe/src/components/pages/add_errand/Add_errand.jsx
+++ errand-buddy-fe/src/components/pages/add_errand/Add_errand.jsx
@@ -1,22 +1,16 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 // import { isAuthenticated } from "../../auth";
 import { useHistory } from "react-router-dom";
-import DateFnsUtils from '@date-io/date-fns'
+import DateFnsUtils from "@date-io/date-fns";
 import axios from "axios";
-import '../../../style/Add_errand.scss'
+import "../../../style/Add_errand.scss";
 
-
-import {
-  DateTimePicker,
-  MuiPickersUtilsProvider,
-} from '@material-ui/pickers'
-
+import { DateTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
 
 function AddErrands(props) {
-
-  const history = useHistory()
-  const {id} = useParams()
+  const history = useHistory();
+  const { id } = useParams();
 
   const [data, setData] = useState({
     category: "",
@@ -25,7 +19,7 @@ function AddErrands(props) {
     pickupLocation: "",
     deliveryLocation: "",
     itemPrice: "",
-    errandFee: ""
+    errandFee: "",
   });
 
   // const [errandData, setErrandData] = useState({
@@ -33,44 +27,40 @@ function AddErrands(props) {
   //   image:"",
   //   price:""
   // })
-  
-  const [pickupDate, setPickupDate] = useState(new Date())
-  const [deliveryDate, setDeliveryDate] = useState(new Date())
 
+  const [pickupDate, setPickupDate] = useState(new Date());
+  const [deliveryDate, setDeliveryDate] = useState(new Date());
 
-  useEffect(()=>{ 
+  useEffect(() => {
     setData({
-    category: "",
+      category: "",
       items: "",
       description: "",
       pickupLocation: "",
       deliveryLocation: "",
       itemPrice: "",
-      errandFee: ""
-    })
-  if (props.location.state){
-  setData(props.location.state.data)
-  console.log(props.location.state.data)
-   }
-  },[id])
-
+      errandFee: "",
+    });
+    if (props.location.state) {
+      setData(props.location.state.data);
+      console.log(props.location.state.data);
+    }
+  }, [id]);
 
   const [image, setImage] = useState({
-      image: ""
+    image: "",
   });
 
-  function uploadImage (event) {
-    setImage ({
-      
-      newImage: event.target.files[0]
-
-    })
+  function uploadImage(event) {
+    setImage({
+      newImage: event.target.files[0],
+    });
   }
 
   // function updateErrandData (info) { //this needs to be checked
-    
+
   //   const { name, value } = info
-    
+
   //   setErrandData((prevData) => {
   //     return {
   //       ...prevData,
@@ -91,221 +81,257 @@ function AddErrands(props) {
   }
 
   //Loading state; loading a spinner to render conditionally
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleClick(event) {
     event.preventDefault();
 
-    setIsLoading(true)
-    console.log(isLoading)
+    setIsLoading(true);
+    console.log(isLoading);
 
-    let formData = new FormData()
-    formData.append ("items", data.items)
-    formData.append("category", data.category)
-    formData.append('description', data.description)
-    formData.append('pickupLocation', data.pickupLocation)
-    formData.append('deliveryLocation', data.deliveryLocation)
-    formData.append('pickupTime', pickupDate)
-    formData.append('deliveryTime', deliveryDate)
-    formData.append('itemPrice', data.itemPrice)
-    formData.append('errandFee', data.errandFee)
-    formData.append('image', image.newImage)
+    let formData = new FormData();
+    formData.append("items", data.items);
+    formData.append("category", data.category);
+    formData.append("description", data.description);
+    formData.append("pickupLocation", data.pickupLocation);
+    formData.append("deliveryLocation", data.deliveryLocation);
+    formData.append("pickupTime", pickupDate);
+    formData.append("deliveryTime", deliveryDate);
+    formData.append("itemPrice", data.itemPrice);
+    formData.append("errandFee", data.errandFee);
+    formData.append("image", image.newImage);
 
-    console.log(formData)
-
-    props.location.state?.data===undefined ? (axios
-      .post("http://localhost:4000/api/users/create-errand", 
-        formData,
-        {
-          headers: {
-            'x-auth-token': localStorage.getItem('jwt'),
-            "Content-Type": "multipart/form-data" 
-          }
-        }
-      ).then (response => {
-
-        history.push ({
-          pathname: `/stripe/payment`, state: {errandData: response.data.errandInfo}
-      })
-      }))
-      : (axios
-      .patch(`http://localhost:4000/api/errands/${props.match.params.id}/update`, 
-        formData,
-        {
-          headers: {
-            'x-auth-token': localStorage.getItem('jwt'),
-            "Content-Type": "multipart/form-data" 
-          }
-        }
-      )
-      .then(response => {
-        console.log('2', response)
-        if (response.data.priceChange) {
-          history.push ({
-            pathname: `/stripe/payment`, state: {errandData: response.data.errandInfo}
-        })
-
-        } else { history.push({
-          pathname: '/'
-          }) 
-        }
-
-      })
-      .catch(err => {
-        console.log(err.data);
-        
-      })
-      .finally (() => {
-        setIsLoading(false)
-      }))
+    if (
+      data.items &&
+      data.category &&
+      data.description &&
+      data.pickupLocation &&
+      data.deliveryLocation &&
+      data.itemPrice &&
+      data.errandFee &&
+      pickupDate &&
+      deliveryDate &&
+      image.newImage
+    ) {
+      props.location.state?.data === undefined
+        ? axios
+            .post("http://localhost:4000/api/users/create-errand", formData, {
+              headers: {
+                "x-auth-token": localStorage.getItem("jwt"),
+                "Content-Type": "multipart/form-data",
+              },
+            })
+            .then((response) => {
+              history.push({
+                pathname: `/stripe/payment`,
+                state: { errandData: response.data.errandInfo },
+              });
+            })
+        : axios
+            .patch(
+              `http://localhost:4000/api/errands/${props.match.params.id}/update`,
+              formData,
+              {
+                headers: {
+                  "x-auth-token": localStorage.getItem("jwt"),
+                  "Content-Type": "multipart/form-data",
+                },
+              }
+            )
+            .then((response) => {
+              console.log("2", response);
+              if (response.data.priceChange) {
+                history.push({
+                  pathname: `/stripe/payment`,
+                  state: { errandData: response.data.errandInfo },
+                });
+              } else {
+                history.push({
+                  pathname: "/",
+                });
+              }
+            })
+            .catch((err) => {
+              console.log(err.data);
+            })
+            .finally(() => {
+              setIsLoading(false);
+            });
+    } else {
+      window.alert("All field required")
+      setIsLoading(false);
+    }
   }
 
   return (
-    
-      <div className="add-row" style={{ backgroundImage: `url(${process.env.PUBLIC_URL + "/create-background.jpg"})`,  backgroundRepeat: "no-repeat", backgroundSize:"cover"}} >
-        <div className="add-container">
-        
-          <form method="POST" action="" />
-          <h1>{!props.location.state ? "Create an Errand" : "Modify Errand"}</h1>
-          <select
-            className="input mb-3"
-            aria-label=".form-select-lg example"
-            name="category"
-            value={data.category} onChange={handleChange}
-          >
-            <option defaultValue> Select Categories</option>
-            <option value="Grocery">Grocery</option>
-            <option value="Queue">Queue</option>
-            <option value="Pet-Sitting">Pet-sit</option>
-            <option value="Others">Others</option>
-          </select>
+    <div
+      className="add-row"
+      style={{
+        backgroundImage: `url(${
+          process.env.PUBLIC_URL + "/create-background.jpg"
+        })`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="add-container">
+        <form method="POST" action="" />
+        <h1>{!props.location.state ? "Create an Errand" : "Modify Errand"}</h1>
+        <select
+          className="input mb-3"
+          aria-label=".form-select-lg example"
+          name="category"
+          value={data.category}
+          onChange={handleChange}
+          required
+        >
+          <option defaultValue> Select Categories</option>
+          <option value="Grocery">Grocery</option>
+          <option value="Queue">Queue</option>
+          <option value="Pet-Sitting">Pet-sit</option>
+          <option value="Others">Others</option>
+        </select>
 
-          <div className="outer mb-3">
-            <input
-              onChange={handleChange}
-              required
-              type="text"
-              className="input  "
-              id="items"
-              name="items"
-              value={data.items}
-              placeholder="Summary of Errand"
-            />
-          </div>
-
-          <div className="outer mb-3">
-
-            <input
-              onChange={uploadImage}
-              className="input"
-              id="image"
-              type="file"
-              name="newImage"
-              placeholder="Upload an image"
-            />
-          </div>
-          <div className="outer textbox mb-3">
-            <textarea
-              rows="5"
-              onChange={handleChange}
-              required
-              type="text"
-              className="input"
-              id="description"
-              name="description"
-              placeholder="Description of Errand"
-              value={data.description}
-            />
-          </div>
-
-          <div className="outer mb-3">
-            <input
-              onChange={handleChange}
-              required
-              type="text"
-              className="input"
-              id="pickupLocation"
-              name="pickupLocation"
-              placeholder="Postal code of errand/pickup location"
-              value={data.pickupLocation}
-            />
-          </div>
-
-          <div className="outer mb-3">
-            <input
-              onChange={handleChange}
-              required
-              type="text"
-              className="input"
-              id="deliveryLocation"
-              name="deliveryLocation"
-              placeholder="Postal code of delivery/ending location"
-              value={data.deliveryLocation}
-            />
-          </div>
-          <div className="outer date-inputs mb-3">
-            <div className="inputDate">
-              <MuiPickersUtilsProvider  utils={DateFnsUtils}>
-                <DateTimePicker label="Start/Pick Up Time" id="pickupTime" name="pickupTime" value={pickupDate} onChange={date => setPickupDate(date)} />
-              </MuiPickersUtilsProvider>
-            </div>
-            <div className="inputDate">
-              <MuiPickersUtilsProvider  utils={DateFnsUtils}>
-                <DateTimePicker label="End/Delivery Time" id="deliveryTime" name="deliveryTime" value={deliveryDate} onChange={date => setDeliveryDate(date)}/>
-              </MuiPickersUtilsProvider>
-            </div>
-          </div>
-
-          <div className="outer moneyDisplay mb-3">
-            <input
-              onChange={handleChange}
-              required
-              type="number"
-              className="input"
-              id="itemPrice"
-              name="itemPrice"
-              placeholder="Price of Items being purchased (if applicable)"
-              value={data.price}
-            />
-          </div>
-          <div className="outer moneyDisplay mb-3">
-            <input
-              onChange={handleChange}
-              required
-              type="number"
-              className="input"
-              id="errandFee"
-              name="errandFee"
-              placeholder="Amount you are willing to pay for the errand"
-              value={data.errandFee}
-            />
-          </div>
-
-          <br />
-
-          { isLoading ?
-            (
-              <button
-                onClick={handleClick}
-                className="inputButton"
-              disabled> {!props.location.state ? "Add Errand" : "Modify Details"}<i class="fas fa-spinner fa-spin"></i>
-              </button>
-            )
-          :
-            (
-              <button
-              onClick={handleClick}
-              className="inputButton"
-              > {!props.location.state ? "Add Errand" : "Modify Details"}
-              
-              </button>
-
-            )
-          }
-
+        <div className="outer mb-3">
+          <input
+            onChange={handleChange}
+            required
+            type="text"
+            className="input  "
+            id="items"
+            name="items"
+            value={data.items}
+            placeholder="Summary of Errand"
+          />
         </div>
-      </div>
 
+        <div className="outer mb-3">
+          <input
+            onChange={uploadImage}
+            required
+            className="input"
+            id="image"
+            type="file"
+            name="newImage"
+            placeholder="Upload an image"
+          />
+        </div>
+        <div className="outer textbox mb-3">
+          <textarea
+            rows="5"
+            onChange={handleChange}
+            required
+            type="text"
+            className="input"
+            id="description"
+            name="description"
+            placeholder="Description of Errand"
+            value={data.description}
+          />
+        </div>
+
+        <div className="outer mb-3">
+          <input
+            onChange={handleChange}
+            required
+            type="text"
+            className="input"
+            id="pickupLocation"
+            name="pickupLocation"
+            placeholder="Postal code of errand/pickup location"
+            value={data.pickupLocation}
+          />
+        </div>
+
+        <div className="outer mb-3">
+          <input
+            onChange={handleChange}
+            required
+            type="text"
+            className="input"
+            id="deliveryLocation"
+            name="deliveryLocation"
+            placeholder="Postal code of delivery/ending location"
+            value={data.deliveryLocation}
+          />
+        </div>
+        <div className="outer date-inputs mb-3">
+          <div className="inputDate">
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <DateTimePicker
+                label="Start/Pick Up Time"
+                id="pickupTime"
+                name="pickupTime"
+                value={pickupDate}
+                onChange={(date) => setPickupDate(date)}
+              />
+            </MuiPickersUtilsProvider>
+          </div>
+          <div className="inputDate">
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <DateTimePicker
+                label="End/Delivery Time"
+                id="deliveryTime"
+                name="deliveryTime"
+                value={deliveryDate}
+                onChange={(date) => setDeliveryDate(date)}
+              />
+            </MuiPickersUtilsProvider>
+          </div>
+        </div>
+
+        <div className="outer moneyDisplay mb-3">
+          <input
+            onChange={handleChange}
+            required
+            type="number"
+            className="input"
+            id="itemPrice"
+            name="itemPrice"
+            placeholder="Price of Items being purchased (if applicable)"
+            value={data.itemPrice}
+          />
+        </div>
+        <div className="outer moneyDisplay mb-3">
+          <input
+            onChange={handleChange}
+            required
+            type="number"
+            className="input"
+            id="errandFee"
+            name="errandFee"
+            placeholder="Amount you are willing to pay for the errand"
+            value={data.errandFee}
+          />
+        </div>
+
+        <br />
+
+        {isLoading ? (
+          <button onClick={handleClick} className="inputButton" disabled>
+            {" "}
+            {!props.location.state ? "Add Errand" : "Modify Details"}
+            <i class="fas fa-spinner fa-spin"></i>
+          </button>
+        ) : (
+          <button onClick={handleClick} className="inputButton">
+            {" "}
+            {!props.location.state ? "Add Errand" : "Modify Details"}
+          </button>
+        )}
+
+        {id && (
+          <button
+            onClick={() => {
+              history.goBack();
+            }}
+            className="inputButton"
+          >
+            Back to Previous
+          </button>
+        )}
+      </div>
+    </div>
   );
 }
 
--- errand-buddy-fe/src/components/pages/home/Home.jsx
+++ errand-buddy-fe/src/components/pages/home/Home.jsx
@@ -7,20 +7,19 @@ import "../../../style/Home.scss";
 const Home = () => {
   const [data, setData] = useState([]);
 
-
   //For Search Bar
-  const [searchField, setSearchField] = useState("") 
-
-  let filteredErrands = data.filter( item =>
-    item.items.toLowerCase().includes(searchField.toLowerCase())
-  )
-
+  const [searchField, setSearchField] = useState("");
   //For Sort Bar
-  const [sortedField, setSortedField] = useState([])
+  const [sortedField, setSortedField] = useState("items");
 
+  let filteredErrands = data.filter((item) => 
+    typeof item[sortedField] == Number
+      ? item[sortedField].includes(searchField)
+      : item[sortedField].toLowerCase().includes(searchField.toLowerCase())
+  );
 
   const userId = localStorage.getItem("userId");
-  
+
   useEffect(() => {
     axios
       .get("http://localhost:4000/")
@@ -28,7 +27,6 @@ const Home = () => {
       .then(function (response) {
         // handle success
         setData(response.data.errands);
-
       })
       .catch(function (error) {
         // handle error
@@ -39,50 +37,86 @@ const Home = () => {
       });
   }, []);
 
+  useEffect(() => {
+    console.log(data, "filter");
+    console.log(sortedField);
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(e.target.select.value);
+  };
 
   return (
-        
-      <div className="home-body" style={{ backgroundImage: `url(${process.env.PUBLIC_URL + "/home-background.jpg"})`,  backgroundRepeat: "no-repeat", backgroundSize:"cover"}}>
-        <div className="home-headers">
-          <h1>Errand Buddy</h1>
-        </div>
-        <div className="main-container mt-5" >
-          <h2 className="mb-4">Available Errands! </h2>
-          <input 
-            type='search' 
-            placeholder="Search the Errands"
-            onChange={e => {setSearchField(e.target.value)} }
-          />
-          <div id="errand-container" className="errand-container row ">
-            {filteredErrands.map((e) => {
-                if (userId !== e.user_id) {
-                  return (
-                    <div className="errand-card mb-3" key={e._id}>
-                      <div className="card-image">
-                        <img src={e.image} alt="Item" />
-                      </div>
-                      <div className="card-body">
-                        <h5 className="card-header">{e.username}</h5>
-                        <Link
-                          to={{ pathname: `/${e._id}`, state: { e } }}
-                          className=""
-                          href=""
-                        >
-                          <h5 className="item-details">{e.items}</h5>
-                        </Link>
-                        <p>Errand Fee: ${e.errandFee}</p>
-                        <p>Pickup At: {e.pickupLocation}</p>
-                        <p>Deliver To: {e.deliveryLocation}</p>
-                      </div>
-                      <Likes errandId={e._id} userId={userId} />
-                    </div>
-                  );
-                }
-              })}
-          </div>
+    <div
+      className="home-body"
+      style={{
+        backgroundImage: `url(${
+          process.env.PUBLIC_URL + "/home-background.jpg"
+        })`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="home-headers">
+        <h1>Errand Buddy</h1>
+      </div>
+      <div className="main-container mt-5">
+        <h2 className="mb-4">Available Errands! </h2>
+        <input
+          name="input"
+          value={searchField}
+          type="search"
+          placeholder="Search the Errands"
+          onChange={(e) => {
+            setSearchField(e.target.value);
+          }}
+        />
+        <select
+          class="form-select form-select-lg mb-3"
+          aria-label=".form-select-lg example"
+          name="select"
+          onChange={(e) => setSortedField(e.target.value)}
+        >
+          <option defaultValue value="items">
+            {" "}
+            Items{" "}
+          </option>
+          {/* <option value="errandFee">Errand Fee</option> */}
+          <option value="username">User Name</option>
+          {/* <option value="itemPrice">Item Price</option> */}
+        </select>
+
+        <div id="errand-container" className="errand-container row ">
+          {filteredErrands.map((e) => {
+            if (userId !== e.user_id) {
+              return (
+                <div className="errand-card mb-3" key={e._id}>
+                  <div className="card-image">
+                    <img src={e.image} alt="Item" />
+                  </div>
+                  <div className="card-body">
+                    <h5 className="card-header">{e.username}</h5>
+                    <Link
+                      to={{ pathname: `/${e._id}`, state: { e } }}
+                      className=""
+                      href=""
+                    >
+                      <h5 className="item-details">{e.items}</h5>
+                    </Link>
+                    <p>Errand Fee: ${e.errandFee}</p>
+                    <p>Pickup At: {e.pickupLocation}</p>
+                    <p>Deliver To: {e.deliveryLocation}</p>
+                  </div>
+                  <Likes errandId={e._id} userId={userId} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
-
+    </div>
   );
 };
 
